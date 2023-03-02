6d3883528ce552eb24d86a7bc0d09976e920e680
Xlaperr <88415760+Xlaperr@users.noreply.github.com>
2023-03-02 20:03:42 +0700

    feat: init back-end

---
+++ back-end/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+app.use(cors())
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
