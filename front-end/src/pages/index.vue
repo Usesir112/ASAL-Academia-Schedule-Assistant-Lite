1aa41d23cd9a01634870c0e1f4c4544db6ee46bb
Super_Octo_Potato_44 <53961723+Usesir112@users.noreply.github.com>
2023-03-02 18:39:08 +0700

    Change version number and description in Login Page

--- front-end/src/pages/index.vue
+++ front-end/src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="login-card">
     <!-- Version -->
     <div class="text-right mr-3 text-disable-100">
-      <p class="kanit-600-16">v.0.1.0</p>
+      <p class="kanit-600-16">v.0.1.16</p>
     </div>
 
     <!-- Content -->
@@ -12,21 +12,21 @@
         <img src="@/assets/images/asap-logo.svg" alt="ASAP Logo" />
       </div>
 
-      <!-- Text line one -->
-      <div class="text-disable kanit-600-24 mt-5">
-        <p>Welcome to</p>
+      <!-- Text line two -->
+      <div class="text-it kanit-600-24 mt-5">
+        <p>Academia Schedule Assistant Pro</p>
       </div>
 
-      <!-- Text line two -->
-      <div class="text-it kanit-600-24 mt-3">
-        <p>Academia Schedule Assistant Pro</p>
+      <!-- Text line one -->
+      <div class="text-disable kanit-600-16 mt-3">
+        <p>ระบบช่วยจัดการแผนการเรียนการสอนประจำหลักสูตร</p>
       </div>
 
       <!-- Login Button -->
       <div class="mt-5">
         <NuxtLink to="/tablesView" :class="`btn-outline-it`">
           <img src="@/assets/icons/google-icon.svg" alt="Google Logo" />
-          <span class="ml-5">Log in with Google</span>
+          <span class="ml-5">เข้าสู่ระบบด้วย Google</span>
         </NuxtLink>
       </div>
     </div>
