99927d0db1e187ca070ffb927239c6f5252ecabf
Xlaperr <88415760+Xlaperr@users.noreply.github.com>
2023-03-03 11:11:57 +0700

    Update login-layout.vue

--- front-end/src/layouts/login-layout.vue
+++ front-end/src/layouts/login-layout.vue
@@ -1,13 +1,10 @@
 <template>
   <Head>
-    <title>
-      เข้าสู่ระบบ | ASAP
-    </title>
+    <title>เข้าสู่ระบบ | ASAL</title>
   </Head>
   <div>
     <!-- Top Vertor and Logo -->
     <div>
-
       <!-- IT KMITL Logo  -->
       <div class="absolute mt-3 ml-3">
         <img
@@ -28,7 +25,8 @@
         >
           <path
             d="M151.5 89.5C114.374 54.5329 43 4.5 0 -0.5H291V178C225.333 150.333 194.5 130 151.5 89.5Z"
-            fill="#FFE3E8" id="top-right"
+            fill="#FFE3E8"
+            id="top-right"
           />
         </svg>
       </div>
@@ -36,9 +34,9 @@
 
     <!-- Content -->
     <div class="flex h-screen">
-        <div class="m-auto">
-            <slot />
-        </div>
+      <div class="m-auto">
+        <slot />
+      </div>
     </div>
 
     <!-- Vertor Bottom Left -->
@@ -52,7 +50,8 @@
       >
         <path
           d="M143 128C101.181 50.6346 -5.7422 35.1856 -14 0V239H314C292.199 200.44 173 183.5 143 128Z"
-          fill="#BFD8FF" id="bottom-left"
+          fill="#BFD8FF"
+          id="bottom-left"
         />
       </svg>
     </div>
@@ -66,7 +65,7 @@
   fill: $it-100;
 }
 
-#top-right{
-  fill: $bit-100
+#top-right {
+  fill: $bit-100;
 }
 </style>
